f94854b37f7b3d4d7d02f09e83e23c23ba05573a
aflanazeezul-rgb <aflanazeezul@gmail.com>
2026-02-27 10:25:38 +0530

    UC4:Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,33 @@
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        // Original String
-        String input = "madam";
+            // Original String
+            String input = "madam";
 
-        // Variable to store reversed string
-        String reversed = "";
+            // Convert String to Character Array
+            char[] characters = input.toCharArray();
 
-        // Reverse using for loop
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+            // Two-pointer approach
+            int start = 0;
+            int end = characters.length - 1;
+
+            boolean isPalindrome = true;
+
+            while (start < end) {
+
+                if (characters[start] != characters[end]) {
+                    isPalindrome = false;
+                    break;
+                }
+
+                start++;
+                end--;
+            }
+
+            // Display Result
+            System.out.println("Input Text : " + input);
+            System.out.println("Is it a palindrome? : " + isPalindrome);
         }
 
-        // Compare original and reversed string
-        boolean isPalindrome = input.equals(reversed);
-
-        // Display result
-        System.out.println("Original String  : " + input);
-        System.out.println("Reversed String  : " + reversed);
-        System.out.println("Is it a palindrome? : " + isPalindrome);
-    }
 }
